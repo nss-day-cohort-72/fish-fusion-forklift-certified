0f5957443819a85582c88d5fbec8f1cc4784774d
alwright33 <austinlucaswright@gmail.com>
2024-07-29 13:15:35 -0500

    updated repo

--- restaurant.js
+++ restaurant.js
@@ -1,2 +1,13 @@
 const { mongerInventory } = require("./fishMonger.js")
 
+/* We need to import and invoke the function that is exported by the fishMonger module. 
+This module should also define a function that builds an HTML version of their daily menu. 
+The function should return the generated HTML string and must be named fishMenu. */
+
+
+
+const fishMenu = (mongerInventory) => {
+  for (const fish of mongerInventory) {
+    if( )
+  }
+}
